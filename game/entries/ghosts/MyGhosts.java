ecd456b7b37fd743e783499aead0cc7e2c605dbd
miningold <rynldsrap@gmail.com>
2012-01-21 16:51:19 -0800

    ms pacman core code installed

---
+++ game/entries/ghosts/MyGhosts.java
@@ -0,0 +1,18 @@
+package game.entries.ghosts;
+
+import game.controllers.GhostController;
+import game.core.Game;
+
+/*
+ * This is the class you need to modify for your entry. In particular, you need to
+ * fill in the getActions() method. Any additional classes you write should either
+ * be placed in this package or sub-packages (e.g., game.entries.ghosts.mypackage).
+ */
+public class MyGhosts implements GhostController
+{
+	//Place your game logic here to play the game as the ghosts
+	public int[] getActions(Game game,long timeDue)
+	{
+		return null;
+	}
+}
