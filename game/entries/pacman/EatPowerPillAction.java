374753189bfbf0cfc0827627a4e4e56522e069ff
miningold <rynldsrap@gmail.com>
2012-01-31 19:58:28 -0800

    added visual path to nearest powerpill

--- game/entries/pacman/EatPowerPillAction.java
+++ game/entries/pacman/EatPowerPillAction.java
@@ -1,12 +1,16 @@
 package game.entries.pacman;
 
+import java.awt.Color;
+
 import game.core.Game;
+import game.core.GameView;
 
 public class EatPowerPillAction extends PacManAction {
 
 	@Override
 	int act(Game game) {
 		int dir = game.getNextPacManDir(MyPacMan.nearestPowerPillLoc, true, Game.DM.PATH);
+		GameView.addPoints(game, Color.YELLOW, game.getPath(game.getCurPacManLoc(), MyPacMan.nearestPowerPillLoc));
 		return dir;
 	}
 
