530e7e882b7e7858042e6429bf659fdf3da37b48
Tylor Reynolds <rynldsrap@gmail.com>
2012-01-31 21:53:39 -0800

    removed and updated comments for NearestPillAction

--- game/entries/pacman/NearestPillAction.java
+++ game/entries/pacman/NearestPillAction.java
@@ -1,6 +1,5 @@
 package game.entries.pacman;
 
-import game.core.G;
 import game.core.Game;
 
 class NearestPillAction extends PacManAction {
@@ -14,10 +13,10 @@ class NearestPillAction extends PacManAction {
 		//get all active power pills
 		int[] activePowerPills = game.getPowerPillIndicesActive();
 		
-		//create a target array that includes all ACTIVE pills and power pills
-		//int[] targetsArray = new int[activePills.length+activePowerPills.length];
-		
+		// find closest pill
 		int target = game.getTarget(current, activePills, true, Game.DM.PATH);
+		
+		// find path from pacman to pill
 		int[] path = game.getPath(current, target);
 		
 		boolean powerExists = false;
@@ -34,23 +33,15 @@ class NearestPillAction extends PacManAction {
 			}
 		}
 		
+		// if power pill in path
 		// move away from power pill
 		if (powerExists) {
 			return game.getNextPacManDir(powerLocation, false, Game.DM.PATH);
 		}
 		
-		// Head straight for the pill
+		// otherwise, head straight for the pill
 		else {
 			return game.getNextPacManDir(target, true, Game.DM.PATH);
 		}
-
-		//for(int i=0;i<activePills.length;i++)
-		//	targetsArray[i] = activePills[i];
-		
-		//for(int i = 0; i < activePowerPills.length; i++)
-		//	targetsArray[activePills.length+i] = activePowerPills[i];		
-		
-		//return the next direction once the closest target has been identified
-		//return game.getNextPacManDir(game.getTarget(current, targetsArray, true, G.DM.PATH), true, Game.DM.PATH);
 	}
 }
