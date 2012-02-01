879d161eb33a85b6a28438feedf45e1e8c3c5b07
Tylor Reynolds <rynldsrap@gmail.com>
2012-01-31 21:51:55 -0800

    NearestPillAction now avoids Power Pills

--- game/entries/pacman/NearestPillAction.java
+++ game/entries/pacman/NearestPillAction.java
@@ -15,15 +15,42 @@ class NearestPillAction extends PacManAction {
 		int[] activePowerPills = game.getPowerPillIndicesActive();
 		
 		//create a target array that includes all ACTIVE pills and power pills
-		int[] targetsArray = new int[activePills.length+activePowerPills.length];
+		//int[] targetsArray = new int[activePills.length+activePowerPills.length];
 		
-		for(int i=0;i<activePills.length;i++)
-			targetsArray[i] = activePills[i];
+		int target = game.getTarget(current, activePills, true, Game.DM.PATH);
+		int[] path = game.getPath(current, target);
 		
-		for(int i = 0; i < activePowerPills.length; i++)
-			targetsArray[activePills.length+i] = activePowerPills[i];		
+		boolean powerExists = false;
+		int powerLocation = -1;
+		
+		// Check for a power pill in the path
+		for (int i = 0; i < path.length; i++) {
+			for (int j = 0; j < activePowerPills.length; j++) {
+				if (path[i] == activePowerPills[j]) {
+					powerExists = true;
+					powerLocation = activePowerPills[j];
+					break;
+				}
+			}
+		}
+		
+		// move away from power pill
+		if (powerExists) {
+			return game.getNextPacManDir(powerLocation, false, Game.DM.PATH);
+		}
+		
+		// Head straight for the pill
+		else {
+			return game.getNextPacManDir(target, true, Game.DM.PATH);
+		}
+
+		//for(int i=0;i<activePills.length;i++)
+		//	targetsArray[i] = activePills[i];
+		
+		//for(int i = 0; i < activePowerPills.length; i++)
+		//	targetsArray[activePills.length+i] = activePowerPills[i];		
 		
 		//return the next direction once the closest target has been identified
-		return game.getNextPacManDir(game.getTarget(current, targetsArray, true, G.DM.PATH), true, Game.DM.PATH);
+		//return game.getNextPacManDir(game.getTarget(current, targetsArray, true, G.DM.PATH), true, Game.DM.PATH);
 	}
 }
