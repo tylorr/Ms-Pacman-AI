21f3288b6b32fa43c06b8b95ab309008973d1710
Tylor Reynolds <rynldsrap@gmail.com>
2012-01-31 23:19:05 -0800

    removed legacy code from RunAwayAction

--- game/entries/pacman/RunAwayAction.java
+++ game/entries/pacman/RunAwayAction.java
@@ -1,36 +1,74 @@
 package game.entries.pacman;
 
-import java.awt.Color;
-import java.util.Random;
-
 import game.core.Game;
-import game.core.GameView;
 
 public class RunAwayAction extends PacManAction {
 	
-	Random random = new Random();
+	static final int SHORTEST_PATH = 10;
 
 	@Override
 	int act(Game game) {
-		int[] possibleDirs = game.getPossiblePacManDirs(true);
+		int current = game.getCurPacManLoc();
 		
-		int[] preferredDirs = new int[possibleDirs.length - 1];
+		int[] targets = game.getPowerPillIndices();
+		int[][] paths = new int[4][];
+		int[] ghosts = new int[4];
+		int[] ghostDists = new int[4];
 		
-		int avoidDir = game.getNextPacManDir(game.getCurGhostLoc(MyPacMan.closestGhost), true, Game.DM.PATH);
-		
-		int index = 0;
-		for (int i = 0; i < possibleDirs.length; i++) {
-			if (possibleDirs[i] != avoidDir) {
-				preferredDirs[index] = possibleDirs[i];
-				index++;
-			}
+		// fill paths
+		for (int i = 0; i < targets.length; i++) {
+			paths[i] = game.getPath(current, targets[i]);
 		}
 		
-		int dirIndex = random.nextInt(preferredDirs.length);
+		// fill ghost location array
+		for (int i = 0; i < ghosts.length; i++) {
+			ghosts[i] = game.getCurGhostLoc(i);
+			ghostDists[i] = game.getPathDistance(current, ghosts[i]);
+		}
 		
-		GameView.addPoints(game, Color.RED, game.getPath(game.getCurPacManLoc(), game.getCurGhostLoc(MyPacMan.closestGhost)));
+		boolean skipPath = false;
+		boolean foundPath = false;
+		int targetIndex = -1;
+		for (int path = 0; (path < paths.length) && !foundPath; path++) {
+			
+			// skip the path if we are too close
+			if (paths[path].length < SHORTEST_PATH) skipPath = true;
+			
+			// for each step of path
+			for (int step = 0; (step < paths[path].length) && !skipPath; step++) {
+				
+				//compare against each ghost
+				for (int ghost = 0; ghost < ghosts.length; ghost++) {
+					
+					// ghost is in range
+					if (ghostDists[ghost] >= 0 && ghostDists[ghost] < MyPacMan.CLOSE_DIST) {
+						
+						// ghost is in path
+						if (ghosts[ghost] == paths[path][step]) {
+							skipPath = true;
+							break;
+						} 
+					}
+					
+				}
+			}
+			
+			// if there was no collision
+			if (!skipPath) {
+				targetIndex = path;
+				foundPath = true;
+			}
+			
+			skipPath = false;
+		}
 		
-		return preferredDirs[dirIndex];
+		if (foundPath) {
+			System.out.println("Found a way out");
+			return game.getNextPacManDir(targets[targetIndex], true, Game.DM.PATH);
+		} else {
+			System.out.println("Just running away from closest");
+			return game.getNextPacManDir(game.getCurGhostLoc(MyPacMan.closestGhost), false, Game.DM.PATH);
+		}
 	}
 
 }
